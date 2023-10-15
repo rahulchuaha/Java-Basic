5fd68644fb6ba577884f86c30cbc2c0a19d0be74
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-15 23:08:46 +0530

    java code for print alternate character of a string

---
+++ OddEven.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class OddEven {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            if(i%2 == 0){
+                System.out.println("even");
+            } else{
+                 System.out.println("odd");
+            }
+        }
+    }
+}
---
+++ alternateElements.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class alternateElements {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for(int i=0; i<str.length(); i++){
+            if(i%2 == 0){
+                System.out.print(str.charAt(i));
+            }
+        }
+    }
+}
