c9683b8d6b2f327aafdd985a714b12789a772ff5
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-15 22:31:47 +0530

    java code for print all multiple of 7 till n

---
+++ MultipleOf7.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MultipleOf7  {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<=n; i++){
+            if(i % 7 == 0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
