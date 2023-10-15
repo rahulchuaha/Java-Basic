aef5c0ca50a8d8d22be83e021711b55edca2952f
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-15 23:11:10 +0530

    java code to reverse a given string

---
+++ ReverseString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ReverseString {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for(int i=str.length()-1; i>=0; i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
