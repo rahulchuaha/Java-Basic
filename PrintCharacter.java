7d9080581678c65779dbdcfa4d7b301c7effc6c1
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-15 23:01:40 +0530

    java code for print series a,c,e,g... till less than z

---
+++ PrintCharacter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PrintCharacter {
+
+    public static void main(String[] args) {
+        for(int i=97; i<=122; i++){
+            if(i%2 == 1){
+                System.out.println((char)i);
+            }
+    }
+  }
+}
