048e5ef134dbcf3437be131999bba9f7aa2d2e94
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-15 22:55:12 +0530

    java code for printing character from a till z

---
+++ PrintAToz.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class PrintAToz {
+
+    public static void main(String[] args) {
+         for(char ch='a'; ch<='z'; ch++){
+             System.out.println(ch);
+         }
+    }
+}
