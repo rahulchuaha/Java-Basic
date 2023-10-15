9518fc1f9ed5063c5f41537632b24df922ad3058
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-15 22:28:23 +0530

    java programm for printing table of 4

---
+++ TableOf4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TableOf4  {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=1; i<=10; i++){
+            System.out.println("4x"+i+"="+4*i);
+        }
+    }
+}
