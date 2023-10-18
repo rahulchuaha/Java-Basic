04c41786f13421594d6c4a480b37d21ad5c166ad
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-18 21:57:36 +0530

    code to print 12*n start's pattern

---
+++ Pattern2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Pattern2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<12; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+         
+    }
+}
