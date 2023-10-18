1b20274bea5131acad17a6091c388ecafc48f6b0
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-18 22:27:33 +0530

    java code to print pattern of right angle triangle multiple of 5

---
+++ Pattern8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Pattern8 {
+    public static void swap(int x, int y , int z){
+        int c = x; 
+        x = z;
+        z = y;
+        y = c;
+         System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        swap(x,y,z);
+    }
+        
+}
+ 
